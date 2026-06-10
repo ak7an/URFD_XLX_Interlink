8b92e383245e2e1ad5dc4dcd60d773a5abfb4231
Edward Nichols <nicholstribe@gmail.com>
2026-06-10 13:20:23 -0600

    Recover custom public dashboard source

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,239 +1,165 @@
 <?php
 
-/*
- *  This dashboard is being developed by the DVBrazil Team as a courtesy to
- *  the XLX Multiprotocol Gateway Reflector Server project.
- *  The dashboard is based of the Bootstrap dashboard template.
-*/
+$time = date('Y-m-d H:i:s T');
+$xmlFile = '/var/log/xlxd.xml';
 
-if (file_exists("./pgs/functions.php")) {
-    require_once("./pgs/functions.php");
-} else {
-    die("functions.php does not exist.");
-}
-if (file_exists("./pgs/config.inc.php")) {
-    require_once("./pgs/config.inc.php");
-} else {
-    die("config.inc.php does not exist.");
-}
+$stations = [];
+$peers = [];
 
-if (!class_exists('ParseXML')) require_once("./pgs/class.parsexml.php");
-if (!class_exists('Node')) require_once("./pgs/class.node.php");
-if (!class_exists('xReflector')) require_once("./pgs/class.reflector.php");
-if (!class_exists('Station')) require_once("./pgs/class.station.php");
-if (!class_exists('Peer')) require_once("./pgs/class.peer.php");
-if (!class_exists('Interlink')) require_once("./pgs/class.interlink.php");
+if (is_readable($xmlFile)) {
+    $raw = file_get_contents($xmlFile);
 
-$Reflector = new xReflector();
-$Reflector->SetFlagFile("./pgs/country.csv");
-$Reflector->SetPIDFile($Service['PIDFile']);
-$Reflector->SetXMLFile($Service['XMLFile']);
+    preg_match_all('/<STATION>(.*?)<\/STATION>/s', $raw, $stationBlocks);
+    foreach ($stationBlocks[1] as $block) {
+        preg_match('/<Callsign>(.*?)<\/Callsign>/s', $block, $cs);
+        preg_match('/<Via node>(.*?)<\/Via node>/s', $block, $via);
+        preg_match('/<On module>(.*?)<\/On module>/s', $block, $module);
+        preg_match('/<Via peer>(.*?)<\/Via peer>/s', $block, $peer);
+        preg_match('/<LastHeardTime>(.*?)<\/LastHeardTime>/s', $block, $lh);
 
-$Reflector->LoadXML();
-
-if ($CallingHome['Active']) {
-
-    $CallHomeNow = false;
-    if (!file_exists($CallingHome['HashFile'])) {
-        $Hash = CreateCode(16);
-        $LastSync = 0;
-        $Ressource = @fopen($CallingHome['HashFile'], "w");
-        if ($Ressource) {
-            @fwrite($Ressource, "<?php\n");
-            @fwrite($Ressource, "\n" . '$LastSync = 0;');
-            @fwrite($Ressource, "\n" . '$Hash     = "' . $Hash . '";');
-            @fwrite($Ressource, "\n\n" . '?>');
-            @fclose($Ressource);
-            @exec("chmod 777 " . $CallingHome['HashFile']);
-            $CallHomeNow = true;
-        }
-    } else {
-        include($CallingHome['HashFile']);
-        if ($LastSync < (time() - $CallingHome['PushDelay'])) {
-            $Ressource = @fopen($CallingHome['HashFile'], "w");
-            if ($Ressource) {
-                @fwrite($Ressource, "<?php\n");
-                @fwrite($Ressource, "\n" . '$LastSync = ' . time() . ';');
-                @fwrite($Ressource, "\n" . '$Hash     = "' . $Hash . '";');
-                @fwrite($Ressource, "\n\n" . '?>');
-                @fclose($Ressource);
-            }
-            $CallHomeNow = true;
-        }
+        $stations[] = [
+            'callsign' => trim($cs[1] ?? ''),
+            'via' => trim($via[1] ?? ''),
+            'module' => trim($module[1] ?? ''),
+            'peer' => trim($peer[1] ?? ''),
+            'lastheard' => trim($lh[1] ?? '')
+        ];
     }
 
-    if ($CallHomeNow || isset($_GET['callhome'])) {
-        $Reflector->SetCallingHome($CallingHome, $Hash);
-        $Reflector->ReadInterlinkFile();
-        $Reflector->PrepareInterlinkXML();
-        $Reflector->PrepareReflectorXML();
-        $Reflector->CallHome();
+    preg_match_all('/<PEER>(.*?)<\/PEER>/s', $raw, $peerBlocks);
+    foreach ($peerBlocks[1] as $block) {
+        preg_match('/<Callsign>(.*?)<\/Callsign>/s', $block, $cs);
+        preg_match('/<LinkedModule>(.*?)<\/LinkedModule>/s', $block, $module);
+        preg_match('/<Protocol>(.*?)<\/Protocol>/s', $block, $proto);
+
+        $peers[] = [
+            'callsign' => trim($cs[1] ?? ''),
+            'module' => trim($module[1] ?? ''),
+            'protocol' => trim($proto[1] ?? '')
+        ];
     }
-} else {
-    $Hash = "";
 }
+
+function nice_time($zulu)
+{
+    if ($zulu == '') return '';
+    $ts = strtotime($zulu);
+    if (!$ts) return $zulu;
+    return date('Y-m-d H:i', $ts);
+}
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="<?php echo $PageOptions['MetaDescription']; ?>"/>
-    <meta name="keywords" content="<?php echo $PageOptions['MetaKeywords']; ?>"/>
-    <meta name="author" content="<?php echo $PageOptions['MetaAuthor']; ?>"/>
-    <meta name="revisit" content="<?php echo $PageOptions['MetaRevisit']; ?>"/>
-    <meta name="robots" content="<?php echo $PageOptions['MetaAuthor']; ?>"/>
+<meta charset="utf-8">
+<title>URF277 Public Dashboard</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <title><?php echo str_replace("XLX", "URF", $Reflector->GetReflectorName()); ?> Universal Reflector</title>
-    <link rel="icon" href="./favicon.ico" type="image/vnd.microsoft.icon">
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/dashboard.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <?php
-
-    if ($PageOptions['PageRefreshActive']) {
-        echo '
-   <script src="./js/jquery-1.12.4.min.js"></script>
-   <script>
-      var PageRefresh;
-
-      function ReloadPage() {
-         $.get("./index.php'.(isset($_GET['show'])?'?show='.$_GET['show']:'').'", function(data) {
-            var BodyStart = data.indexOf("<bo"+"dy");
-            var BodyEnd = data.indexOf("</bo"+"dy>");
-            if ((BodyStart >= 0) && (BodyEnd > BodyStart)) {
-               BodyStart = data.indexOf(">", BodyStart)+1;
-               $("body").html(data.substring(BodyStart, BodyEnd));
-            }
-         })
-            .always(function() {
-               PageRefresh = setTimeout(ReloadPage, '.$PageOptions['PageRefreshDelay'].');
-            });
-      }';
-
-        if (!isset($_GET['show']) || (($_GET['show'] != 'livequadnet') && ($_GET['show'] != 'reflectors') && ($_GET['show'] != 'interlinks'))) {
-            echo '
-      PageRefresh = setTimeout(ReloadPage, ' . $PageOptions['PageRefreshDelay'] . ');';
-        }
-        echo '
-
-      function SuspendPageRefresh() {
-        clearTimeout(PageRefresh);
-      }
-   </script>';
-    }
-    if (!isset($_GET['show'])) $_GET['show'] = "";
-    ?>
+<style>
+body{background:#0b1118;color:#ffffff;font-family:Arial,sans-serif;margin:0;}
+header{background:#162231;padding:20px;}
+main{padding:20px;}
+.card{background:#162231;border:1px solid #2d425c;border-radius:10px;padding:20px;margin-bottom:20px;max-width:1100px;}
+.good{color:#66ff66;font-weight:bold;}
+.idle{color:#a0a0a0;}
+.grid{display:grid;grid-template-columns:repeat(auto-fit,minmax(140px,1fr));gap:12px;}
+.badge{background:#0f1a25;border:1px solid #2d425c;border-radius:8px;padding:15px;text-align:center;}
+table{width:100%;border-collapse:collapse;}
+th,td{padding:10px;border-bottom:1px solid #2d425c;text-align:left;}
+.small{color:#a0a0a0;font-size:0.9em;}
+</style>
 </head>
+
 <body>
-<?php if (file_exists("./tracking.php")) {
-    include_once("tracking.php");
-} ?>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                    aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <span class="navbar-brand"><?php echo str_replace("XLX", "URF", $Reflector->GetReflectorName()); ?> Universal Reflector</span>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="navbar-info">V#Q<?php echo $Reflector->GetVersion(); ?> - Dashboard
-                    v<?php echo $PageOptions['DashboardVersion']; ?></li>
-                <li class="navbar-info">Service
-                    uptime: <?php echo FormatSeconds($Reflector->GetServiceUptime()); ?></li>
-            </ul>
-        </div>
-    </div>
-</nav>
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-            <ul class="nav nav-sidebar">
-                <li<?php echo (($_GET['show'] == "users") || ($_GET['show'] == "")) ? ' class="active"' : ''; ?>><a
-                            href="./index.php">Last Heard</a></li>
-                <li<?php echo ($_GET['show'] == "repeaters") ? ' class="active"' : ''; ?>><a
-                            href="./index.php?show=repeaters">Links (<?php echo $Reflector->NodeCount(); ?>)
-                        </a></li>
-                <li<?php echo ($_GET['show'] == "peers") ? ' class="active"' : ''; ?>><a href="./index.php?show=peers">Peers
-                        (<?php echo $Reflector->PeerCount(); ?>)</a></li>
-                <li<?php echo ($_GET['show'] == "reflectors") ? ' class="active"' : ''; ?>><a
-                            href="./index.php?show=reflectors">Reflector List</a></li>
-                <li<?php echo ($_GET['show'] == "livequadnet") ? ' class="active"' : ''; ?>><a
-                            href="./index.php?show=livequadnet">QuadNet Live</a></li>
-            </ul>
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+<header>
+<h1>URF277 Reflector Dashboard</h1>
+<div class="small">xlx277.bitbybithams.com / urfd</div>
+</header>
 
-            <?php
-            if ($CallingHome['Active']) {
-                if (!is_readable($CallingHome['HashFile']) && (!is_writeable($CallingHome['HashFile']))) {
-                    echo '
-         <div class="error">
-            your private hash in ' . $CallingHome['HashFile'] . ' could not be created, please check your config file and the permissions for the defined folder.
-         </div>';
-                }
-            }
+<main>
 
-            switch ($_GET['show']) {
-                case 'users'      :
-                    require_once("./pgs/users.php");
-                    break;
-                case 'repeaters'  :
-                    require_once("./pgs/repeaters.php");
-                    break;
-                case 'livequadnet' :
-                    require_once("./pgs/livequadnet.php");
-                    break;
-                case 'peers'      :
-                    require_once("./pgs/peers.php");
-                    break;
-                case 'reflectors' :
-                    require_once("./pgs/reflectors.php");
-                    break;
-                default           :
-                    require_once("./pgs/users.php");
-            }
-
-            ?>
-
-        </div>
-    </div>
+<div class="card">
+<h2>Reflector Status</h2>
+<p><span class="good">ONLINE</span></p>
+<p class="small">Last dashboard update: <?= htmlspecialchars($time) ?></p>
 </div>
 
-<footer class="footer">
-    <div class="container">
-        <p><a href="mailto:<?php echo $PageOptions['ContactEmail']; ?>"><?php echo $PageOptions['ContactEmail']; ?></a>
-        </p>
-    </div>
-</footer>
+<div class="card">
+<h2>Protocols</h2>
+<div class="grid">
+<div class="badge">D-Star<br><span class="good">ONLINE</span></div>
+<div class="badge">DMR<br><span class="good">ONLINE</span></div>
+<div class="badge">YSF<br><span class="good">ONLINE</span></div>
+<div class="badge">NXDN<br><span class="good">ONLINE</span></div>
+<div class="badge">P25<br><span class="good">ONLINE</span></div>
+<div class="badge">M17<br><span class="good">ONLINE</span></div>
+</div>
+</div>
 
-<!-- Bootstrap core JavaScript
- ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-<script src="js/bootstrap.min.js"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="js/ie10-viewport-bug-workaround.js"></script>
+<div class="card">
+<h2>Linked Systems</h2>
+<table>
+<tr><th>System</th><th>Protocol</th><th>Module</th><th>Status</th></tr>
+
+<?php if (count($peers) > 0): ?>
+<?php foreach ($peers as $peer): ?>
+<tr>
+<td><?= htmlspecialchars($peer['callsign']) ?></td>
+<td><?= htmlspecialchars($peer['protocol']) ?></td>
+<td><?= htmlspecialchars($peer['module']) ?></td>
+<td class="good">LINKED</td>
+</tr>
+<?php endforeach; ?>
+<?php else: ?>
+<tr><td colspan="4" class="idle">No linked peers found.</td></tr>
+<?php endif; ?>
+
+</table>
+</div>
+
+<div class="card">
+<h2>Last Heard</h2>
+<table>
+<tr>
+<th>Time</th>
+<th>Callsign</th>
+<th>Via Node</th>
+<th>Module</th>
+<th>Via Peer</th>
+</tr>
+
+<?php if (count($stations) > 0): ?>
+<?php foreach ($stations as $st): ?>
+<tr>
+<td><?= htmlspecialchars(nice_time($st['lastheard'])) ?></td>
+<td><?= htmlspecialchars($st['callsign']) ?></td>
+<td><?= htmlspecialchars($st['via']) ?></td>
+<td><?= htmlspecialchars($st['module']) ?></td>
+<td><?= htmlspecialchars($st['peer']) ?></td>
+</tr>
+<?php endforeach; ?>
+<?php else: ?>
+<tr><td colspan="5" class="idle">No last-heard stations found.</td></tr>
+<?php endif; ?>
+
+</table>
+</div>
+
+<div class="card">
+<h2>Active Streams</h2>
+<div class="grid">
+<div class="badge">D-Star<br><span class="idle">0</span></div>
+<div class="badge">DMR<br><span class="idle">0</span></div>
+<div class="badge">YSF<br><span class="idle">0</span></div>
+<div class="badge">NXDN<br><span class="idle">0</span></div>
+<div class="badge">P25<br><span class="idle">0</span></div>
+<div class="badge">M17<br><span class="idle">0</span></div>
+</div>
+</div>
+
+</main>
+
 </body>
 </html>
